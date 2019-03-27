14c1734a8aad164fa0ea73be839e668a42756c27
Madison Stankevich <madison.stankevich@flatironschool.com>
2019-03-27 16:18:33 -0700

    Random cat generator.

--- app/models/mark.rb
+++ app/models/mark.rb
@@ -1,8 +1,49 @@
+require 'faker'
 class Mark < ApplicationRecord
 
   has_one :cat
   belongs_to :assassin
   has_one :cat_food, through: :assassin
   delegate :id, to: :cat, prefix: true
+  after_save :assign_random_cat
+
+  # private
+
+  def assign_random_cat
+    if !self.cat
+      self.create_cat(image_url: self.catImages.sample, name: self.catName, description: self.catDescription)
+    end
+  end
+
+  def catImages
+    return ["https://cdn.pixabay.com/photo/2017/06/12/19/02/cat-2396473__480.jpg",
+    "https://cdn.pixabay.com/photo/2015/06/03/13/13/cats-796437__480.jpg",
+    "https://cdn.pixabay.com/photo/2012/11/26/13/58/cat-67345__480.jpg",
+    "https://cdn.pixabay.com/photo/2014/09/18/20/17/cat-451377__480.jpg",
+    "https://cdn.pixabay.com/photo/2015/01/31/12/36/cat-618470__480.jpg",
+    "https://cdn.pixabay.com/photo/2014/07/24/18/40/cat-401124__480.jpg",
+    "https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262__480.jpg",
+    "https://cdn.pixabay.com/photo/2015/02/14/10/16/cat-636172__480.jpg",
+    "https://cdn.pixabay.com/photo/2013/10/28/14/30/cat-201855__480.jpg",
+    "https://cdn.pixabay.com/photo/2015/04/16/15/21/cat-725793__480.jpg",
+    "https://cdn.pixabay.com/photo/2016/01/20/13/05/cat-1151519__480.jpg",
+    "https://cdn.pixabay.com/photo/2017/05/31/21/52/cat-2361787__480.jpg",
+    "https://cdn.pixabay.com/photo/2014/10/01/10/46/cat-468232__480.jpg",
+    "https://cdn.pixabay.com/photo/2014/04/29/13/19/cat-334383__480.jpg",
+    "https://cdn.pixabay.com/photo/2014/01/17/14/53/cat-246933__480.jpg",
+    "https://cdn.pixabay.com/photo/2017/05/31/21/46/cats-2361762__480.jpg",
+    "https://cdn.pixabay.com/photo/2017/05/21/22/06/cat-2332444__480.jpg",
+    "https://cdn.pixabay.com/photo/2014/03/30/23/35/cat-301720__480.jpg",
+    "https://cdn.pixabay.com/photo/2017/05/21/22/07/cat-2332451__480.jpg",
+    "https://cdn.pixabay.com/photo/2014/08/03/00/51/kitten-408798__480.jpg"]
+  end
+
+  def catDescription
+    return Faker::Quote.most_interesting_man_in_the_world
+  end
+
+  def catName
+    Faker::Creature::Cat.name
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
 Cat.destroy_all
 Mark.destroy_all
 CatFood.destroy_all
 Assassin.destroy_all
 
-assassin = Assassin.create(name: "Jules and Vincent", image_url: "http://i2.cdn.turner.com/cnn/dam/assets/140922202605-08-pulp-fiction-story-top.jpg", description: "That is a tasty burger", price: "$30,000")
+assassin1 = Assassin.create(name: "Jules and Vincent", image_url: "http://i2.cdn.turner.com/cnn/dam/assets/140922202605-08-pulp-fiction-story-top.jpg", description: "That is a tasty burger", price: "$30,000")
+assassin2 = Assassin.create(name: "John Wick", image_url: "http://exclaim.ca/images/john-wick-chapter-2-poster-big.jpg", description: "That dog was a gift from my wife.", price: "$100,000")
+assassin3 = Assassin.create(name: "Deadpool", image_url: "https://thewatchmancomau.files.wordpress.com/2018/05/deadpool-2-wallpaper-4i-e1526796176729.jpg", description: "Something inappropriate", price: "$69.00")
+assassin4 = Assassin.create(name: "Crazy Joe", image_url: "https://listverse.com/wp-content/uploads/2012/04/50336_45755462571_4089_n.jpg", description: "You can get much further with a kind word and a gun than you can with just a kind word.", price: "$100,000 (*adjusted for inflation)")
+assassin5 = Assassin.create(name: "Luca Brasi", image_url: "https://images.ctfassets.net/23wh7e5ryr18/photo-40262/e11738d29fbf98131f3da205209d48c9/40262-godfather-luca-1.jpg?fm=jpg&fl=progressive&w=900&q=50", description: "The biggest man we've got", price: "$5,000")
+assassin7 = Assassin.create(name: "James Bond", image_url: "https://images-i.jpimedia.uk/imagefetch/c_fill,f_auto,h_640,q_auto:eco,w_960/https://inews.co.uk/wp-content/uploads/2017/05/james-bond.jpg", description: "The name is Bond. James bond", price: "$0.07")
 
-assassin.create_cat_food(name: "Friskies", image_url: "https://www.dollargeneral.com/media/catalog/product/cache/image/700x700/e9c3970ab036de70892d86c6d221abfe/d/e/default_00050000015474_cl.jpg", description: "Seafood Sensations")
-
-mark = assassin.marks.create(name: "Frank", image_url: "https://i.pinimg.com/236x/4a/63/10/4a6310b5f4868d394aff9eb81a2a4645--frank-sinatra-mugshot-celebrity-mugshots.jpg", description: "a singer")
-
-mark.create_cat(name: "fluffy", image_url: "https://ichef.bbci.co.uk/images/ic/720x405/p0517py6.jpg", description: "very fluffy")
-
-assassin2 = Assassin.create(name: "James Bond", image_url: "https://images-i.jpimedia.uk/imagefetch/c_fill,f_auto,h_640,q_auto:eco,w_960/https://inews.co.uk/wp-content/uploads/2017/05/james-bond.jpg", description: "The name is Bond. James bond", price: "$0.07")
-
+assassin1.create_cat_food(name: "Friskies", image_url: "https://www.dollargeneral.com/media/catalog/product/cache/image/700x700/e9c3970ab036de70892d86c6d221abfe/d/e/default_00050000015474_cl.jpg", description: "Seafood Sensations")
 assassin2.create_cat_food(name: "Canned Tuna", image_url: "http://static1.squarespace.com/static/596fb734c534a5fa937b7e75/5ab0164003ce6497ede5c48a/5aa9b6cf71c10b19376b5122/1521071850333/FP222_Tuna+Can.jpg", description: "Chicken of the sea.")
+assassin3.create_cat_food(name: "Cold Pizza", image_url: "https://www.saintclarefestival.net/wp-content/uploads/2018/05/Google-Reuse-Unknown-Cheese-Pizza-Slice.jpg", description: "Eeveryone loves it.")
+assassin4.create_cat_food(name: "Meow Mix", image_url: "https://images-na.ssl-images-amazon.com/images/I/71YURG2o%2BdL._SX425_.jpg", description: "Meow. Meow. Meow. Meow. Meow. Meow. Meow. Meow. Meow.")
+assassin5.create_cat_food(name: "Sashimi", image_url: "https://i.pinimg.com/originals/da/a5/5b/daa55b5a14d5223582aef6cc88c58fd1.jpg", description: "Uncooked salmon.")
+assassin7.create_cat_food(name: "Fancy Feast", image_url: "https://pics.drugstore.com/prodimg/93017/900.jpg", description: "For the fancy beast.")
+
+mark = assassin5.marks.create(name: "Frank", image_url: "https://i.pinimg.com/236x/4a/63/10/4a6310b5f4868d394aff9eb81a2a4645--frank-sinatra-mugshot-celebrity-mugshots.jpg", description: "a singer")
+# mark.create_cat(name: "fluffy", image_url: "https://ichef.bbci.co.uk/images/ic/720x405/p0517py6.jpg", description: "very fluffy")
+# mark.save
